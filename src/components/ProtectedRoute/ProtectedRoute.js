3969d226a876efb91537c3156bf28f5798735561
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-03-07 21:54:23 +0100

    protected route login path issue fixed

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -10,7 +10,7 @@ const ProtectedRoute = ({ children }) => {
     const [user] = useAuthState(auth);
     const location = useLocation();
     if (!user) {
-        return <Navigate to='login' state={{ from: location }} replace />
+        return <Navigate to='/login' state={{ from: location }} replace />
     }
     return children;
 
